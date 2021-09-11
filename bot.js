ad625fe3a674a920e3cbd8a7fdd629a73e1bd9b9
Aleksey Skhomenko <a@skhom.ru>
2021-09-11 15:50:45 +0700

    fix

--- bot.js
+++ bot.js
@@ -19,15 +19,20 @@ bot.on('text', (ctx) => {
 
 bot.on('inline_query', async (ctx) => {
     const result = Math.round((Math.random() * 100) * (Math.random() * 4) * 0.25);
-	const text = `<b>Мой результат: </b> ${ result >= 26 ? "Completed" : "Fail"} (${ 1.5 * result } XP, ${result}%)`;
+	const text = `<b>Мой результат: </b> ${ result >= 26 ? "Completed ✅" : "Fail ❌"} (${ 1.5 * result } XP, ${result}%)`;
 	return await ctx.answerInlineQuery([
+			{
+				type: 'sticker',
+				id: ctx.update.update_id,
+				sticker_file_id: 'CAACAgIAAxkBAAO6YTxbnqeETfSPs4_v-Z6-ga0dnGEAAlwAA2RhcS7JLCyaQaq8TiAE',
+				input_message_content: { message_text: text, parse_mode: 'HTML' }
+			}
+		],
 		{
-			type: 'sticker',
-			id: ctx.update.update_id,
-			sticker_file_id: 'CAACAgIAAxkBAAO6YTxbnqeETfSPs4_v-Z6-ga0dnGEAAlwAA2RhcS7JLCyaQaq8TiAE',
-			input_message_content: { message_text: text, parse_mode: 'HTML' }
+			inline_query_id: ctx.update.inline_query.id,
+			cache_time: 0
 		}
-	])
+	)
 })
 
 bot.launch();
