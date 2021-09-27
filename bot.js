2daaa3e68594219e07bed3836ce53e73fde49bbb
Aleksey Skhomenko <a@skhom.ru>
2021-09-27 18:28:27 +0700

    status cmd

--- bot.js
+++ bot.js
@@ -2,17 +2,71 @@ require('dotenv').config();
 const { Telegraf } = require('telegraf');
 const TIMEOUT = 300000;
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const admRegex = /админ|aдмин|admin|аdmin|adмin/i;
 
 const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || 'https://morfey21bot.herokuapp.com';
 bot.telegram.setWebhook(`${URL}/bot${process.env.BOT_TOKEN}`);
 bot.startWebhook(`/bot${process.env.BOT_TOKEN}`, null, PORT)
 
+const answer = (ctx, msg) => {
+	if (ctx.chat.type === 'private') {
+		ctx.reply(msg);
+	} else {
+		console.log(msg);
+	}
+}
+
 bot.start((ctx) => {
 	if (ctx.chat.type === 'private') {
 		ctx.reply('Просто начни писать @verter21_bot текст на любом канале.');
+		bot.telegram.setMyCommands([
+            { command: 'status', description: 'Изменить статус во флудилке доплывших' }
+        ]);
 	}
-})
+});
+
+// chat id -1001594852516
+bot.command('status', (ctx) => {
+    if (ctx.chat.type != 'private') {
+		const delete_id = ctx.message.message_id;
+		bot.telegram.deleteMessage(-1001594852516, delete_id).then(action => {
+			console.log(`Сообщение [ID:${delete_id}] удалено.`);
+		}).catch(err => {
+			console.log(`Не получилось удалить сообщение [ID:${delete_id}].`);
+		})
+	}
+	const status = ctx.message.text.substring(ctx.message.text.split(' ')[0].length + 1);
+	console.log(ctx.message.from);
+	console.log('status', status);
+	if (status) {
+		bot.telegram.promoteChatMember(-1001594852516, ctx.message.from.id, {can_pin_messages: true}).then(action => {
+			if (status.length <= 16 && !status.replace(/\s/gm, '').match(admRegex)) {
+				bot.telegram.setChatAdministratorCustomTitle(-1001594852516, ctx.message.from.id, status).then(action => {
+					answer(ctx, `Твой статус успешно изменён на "${status}".`);
+				}).catch(err => {
+					console.log(err);
+					answer(ctx, `Не получилось изменить твой статус.
+Причина: ${err.response.description}`);
+				});
+			} else {
+				answer(ctx, `n/a`);
+			}
+		}).catch(err => {
+			console.log(err);
+			answer(ctx, `Не получилось изменить права пользователя.
+Причина: ${err.response.description}`);
+			});	
+	} else {
+		bot.telegram.promoteChatMember(-1001594852516, ctx.message.from.id, false).then(action => {
+			answer(ctx, 'Твой статус удалён.');
+		}).catch(err => {
+			console.log(err);
+			answer(ctx, `Не получилось изменить права пользователя.
+Причина: ${err.response.description}`);
+			});
+        }
+});
 
 bot.on('sticker', (ctx) => {
     // console.log('ctx', ctx);
@@ -38,6 +92,6 @@ bot.on('inline_query', async (ctx) => {
 			cache_time: 0
 		}
 	)
-})
+});
 
 bot.launch();
