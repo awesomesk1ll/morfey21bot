3654904e4750af6860f110172b53b17c3bed6078
Aleksey Skhomenko <a@skhom.ru>
2021-10-12 15:44:33 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -98,16 +98,16 @@ bot.on('inline_query', async (ctx) => {
 function checkUpdate() {
     request({ method: 'GET', uri: 'https://archive.sendpulse.com/u/NzA5OTc3OQ==/Acb6c5583/', gzip: true}, function (error, response, body) {
         let rows = body.split(/<div class="send-date cell">/gm);
-        let first = rows[1].replace(/\s*/, '').split('г.')[0];
+        let first = rows[1].replace(/\s*/, '').split(' г.')[0];
         if (first === '8 октября 2021') { // ничего не поменялось
-		bot.telegram.sendMessage(424895349, 'А вот и письма полетели! Время для создания опроса xD');
+		console.log('Всё по старому.');
+		bot.telegram.sendMessage(424895349, '123');
 		bot.telegram.sendSticker(424895349, 'CAACAgIAAxkBAAO6YTxbnqeETfSPs4_v-Z6-ga0dnGEAAlwAA2RhcS7JLCyaQaq8TiAE');
         } else { // новая рассылочка Оп оп.
 		// bot.telegram.sendMessage(-1001594852516, 'А вот и письма полетели!');
 		bot.telegram.sendMessage(424895349, 'А вот и письма полетели! Время для создания опроса xD');
 		bot.telegram.sendSticker(424895349, 'CAACAgIAAxkBAAO6YTxbnqeETfSPs4_v-Z6-ga0dnGEAAlwAA2RhcS7JLCyaQaq8TiAE');
-		console.log('test-1', first, JSON.stringify({key: first}));
-		// clearInterval(handle);
+		clearInterval(handle);
         }
     });
 }
